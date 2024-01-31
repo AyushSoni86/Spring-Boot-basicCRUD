2578582705a37dd6ca96477136c530c38476214b
Ayush Soni <ayush.soni2464@gmail.com>
2024-01-31 15:45:38 +0530

    Initial commit

---
+++ src/main/java/com/example/basicCRUD/model/Book.java
@@ -0,0 +1,25 @@
+package com.example.basicCRUD.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Books")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String author;
+
+}
